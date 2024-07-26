61817fa8b2f2ef399882159c6b3fcdf969e11a1d
masaaya <156692516+mkanetsuna@users.noreply.github.com>
2024-07-26 17:28:36 +0000

    first commit

---
+++ build/CMakeFiles/my_cpp_web_project.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/my_cpp_web_project.dir/src/main.cpp.o"
+  "my_cpp_web_project"
+  "my_cpp_web_project.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/my_cpp_web_project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
